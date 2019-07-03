3bdc1651a8f5d0c26618e369e783a0c6fc5a8ab2
Bruno Veiga <bspveiga@gmail.com>
2019-07-03 10:50:25 -0500

    Adding ability to copy non js and non css files to distribution

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,18 @@ gulp.task('clean', function() {
 	return del(['dist/**'], {force: true});
 });
 
-gulp.task('default', gulp.series('clean'));
+/*----- Copy Liquid Files -----*/
+gulp.task('copy', function() {
+	return gulp.src([
+		'./src/assets/*',
+		'./src/config/settings_schema.json',
+		'./src/layout/*',
+		'./src/locales/*',
+		'./src/sections/*',
+		'./src/snippets/*',
+		'./src/templates/**/*'
+	], {base: './src/'})
+	.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', gulp.series('clean', 'copy'));
